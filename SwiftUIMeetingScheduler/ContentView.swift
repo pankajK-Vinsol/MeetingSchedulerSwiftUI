de9dddccd3780cb91e419721bebfc6909755c4d5
Pankaj Kumar <pankaj.kumar@vinsol.com>
2020-07-02 14:11:10 +0530

    Fix home screen with static list

--- SwiftUIMeetingScheduler/ContentView.swift
+++ SwiftUIMeetingScheduler/ContentView.swift
@@ -9,6 +9,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    init() {
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor.white]
+        UINavigationBar.appearance().barTintColor = UIColor(red: 0.12941177, green: 0.5686275, blue: 0.44705883, alpha: 1.0)
+    }
+    
+    @State private var dateString = ""
+    @State private var date = NSDate()
+    @State private var disableScheduleButton = false
     
     struct ButtonTextStyle: ViewModifier {
         func body(content: Content) -> some View {
@@ -26,38 +34,76 @@ struct ContentView: View {
     }
     
     var body: some View {
-        ZStack(alignment: .top) {
-            HStack {
-                Button(action: {
-                }) {
-                    Text("PREV").modifier(ButtonTextStyle())
-                        .frame(height: 80.0)
-                }
-                .padding(.leading, 10)
-                Spacer()
-                Spacer()
-                Text("07-01-2020").modifier(ButtonTextStyle())
-                Spacer()
-                Spacer()
-                Button(action: {
-                }) {
-                    Text("NEXT").modifier(ButtonTextStyle())
-                }
-                .padding(.trailing, 10)
+        VStack {
+            List {
+                MeetingRow()
             }
+            NavigationLink(destination: SchedulerView()) {
+                Text("SCHEDULE COMPANY MEETING").modifier(ButtonTextStyle())
+                    .frame(width: 280, height: 40, alignment: .center)
+            }
+            .disabled(disableScheduleButton)
             .modifier(BackgroundColorStyle())
-            
-            VStack {
-                List {
-                    MeetingRow()
-                }
-                
-                NavigationLink(destination: SchedulerView()) {
-                    Text("SCHEDULE COMPANY MEETING").modifier(ButtonTextStyle())
-                        .frame(width: 280, height: 40, alignment: .center)
-                }
-                .modifier(BackgroundColorStyle())
-            }
+        }
+        .onAppear(perform: setInitialNavigationTitle)
+        .navigationBarTitle(Text(dateString), displayMode: .inline)
+        .navigationBarItems(leading: Button(action: {
+            self.showPreviousDate()
+        }) {
+            Text("PREV").modifier(ButtonTextStyle())
+        },
+        trailing: Button(action: {
+            self.showNextDate()
+        }) {
+            Text("NEXT").modifier(ButtonTextStyle())
+        })
+    }
+    
+    private func setInitialNavigationTitle() {
+        let weekDay = Calendar.current.component(.weekday, from: date as Date)
+        if weekDay == 7 {
+            date = Calendar.current.date(byAdding: .day, value: 2, to: date as Date)! as NSDate
+        } else if weekDay == 1 {
+            date = Calendar.current.date(byAdding: .day, value: 1, to: date as Date)! as NSDate
+        }
+        dateString = convertDateAsString(dateString: date)
+    }
+    
+    private func showPreviousDate() {
+        let weekDay = Calendar.current.component(.weekday, from: date as Date)
+        if weekDay == 2 {
+            date = Calendar.current.date(byAdding: .day, value: -3, to: date as Date)! as NSDate
+        } else {
+            date = Calendar.current.date(byAdding: .day, value: -1, to: date as Date)! as NSDate
+        }
+        dateString = convertDateAsString(dateString: date)
+        canScheduleMeetingForDate(date)
+    }
+    
+    private func showNextDate() {
+        let weekDay = Calendar.current.component(.weekday, from: date as Date)
+        if weekDay == 6 {
+            date = Calendar.current.date(byAdding: .day, value: 3, to: date as Date)! as NSDate
+        } else {
+            date = Calendar.current.date(byAdding: .day, value: 1, to: date as Date)! as NSDate
+        }
+        dateString = convertDateAsString(dateString: date)
+        canScheduleMeetingForDate(date)
+    }
+    
+    private func convertDateAsString(dateString: NSDate) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd-MM-yyyy"
+        return dateFormatter.string(from: dateString as Date)
+    }
+    
+    private func canScheduleMeetingForDate(_ getDate: NSDate) {
+        let checkDate = NSDate()
+        let checkDateString = convertDateAsString(dateString:  NSDate())
+        if getDate.compare(checkDate as Date) == .orderedDescending || dateString == checkDateString {
+            disableScheduleButton = false
+        } else {
+            disableScheduleButton = true
         }
     }
 }
